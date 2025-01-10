33b8c50953220653f2c35bd863805cfadc93a390
minanadynarouz <mina.n.narouz@gmail.com>
2025-01-10 08:59:43 +0200

    Adding hashed password comparisons

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,20 +1,19 @@
 import bcrypt from 'bcrypt';
 
-export async function passwordCheck(passFromWeb, passStoredInDb) {
-  // Simply compare the two passwords as strings
-  const isMatch = passFromWeb === passStoredInDb;
-  return isMatch;
-}
+// export async function passwordCheck(passFromWeb, passStoredInDb) {
+//   // Simply compare the two passwords as strings
+//   const isMatch = passFromWeb === passStoredInDb;
+//   return isMatch;
+// }
 
-
-// export const passwordCheck = async (inputPassword, storedHash) => {
-//   try {
-//     return await bcrypt.compare(inputPassword, storedHash);
-//   } catch (err) {
-//     console.error("Error comparing passwords:", err);
-//     throw err;
-//   }
-// };
+export const passwordCheckHashed = async (inputPassword, storedHash) => {
+  try {
+    return await bcrypt.compare(inputPassword, storedHash);
+  } catch (err) {
+    console.error("Error comparing hashed passwords:", err);
+    throw err;
+  }
+};
 
 export async function hashPassword(password) {
   const saltRounds = 6;
