b70744ed543a5697cb395512f023214495a154c3
minanadynarouz <mina.n.narouz@gmail.com>
2025-01-10 11:07:54 +0200

    Amending session sql database

--- server/server.js
+++ server/server.js
@@ -10,8 +10,7 @@ import addUser from './routes/addUser.js';
 import getFiles from './routes/getFiles.js';
 import downloadFiles from './routes/downloadFiles.js';
 import session from 'express-session';
-import MySQLStore from 'connect-mysql';
-
+import MySQLStore from 'express-mysql-session';
 
 dotenv.config();
 
@@ -20,25 +19,30 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 
-const sessionStore = new MySQLStore({
-  config: {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'session_table',
-  }
-})
+// const options = {
+//   host: "localhost",
+//   user: "root",
+//   password: process.env.MYSQL_PASS,
+//   database: "inland_db",
+// };
 
+// const sessionStore = new MySQLStore(options);
+
+// Configure session middleware
 app.use(
   session({
     key: 'user_email',
     secret: '12Ks32d1dmimiasdanoli3213-12-@_@_#Finisrong',
-    store: sessionStore,
+    // store: sessionStore,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false, httpOnly: true, maxAge: 3600000 * 4 }, // Set to true if using HTTPS
+    cookie: {
+      secure: false, // Set to true if using HTTPS
+      httpOnly: true,
+      maxAge: 3600000 * 4, // 4 hours
+    },
   })
-)
+);
 
 app.use(cors({
   origin: 'http://localhost:5173',
