3ca9a8002a19f0c918eb000df239928ae135f304
minanadynarouz <mina.n.narouz@gmail.com>
2025-01-10 08:36:55 +0200

    Adding express-validator to validate and sanitize user input in login page

--- server/routes/loginRoutes.js
+++ server/routes/loginRoutes.js
@@ -1,39 +1,45 @@
 import express from 'express';
 import { pool } from '../config/db.js'
 import { passwordCheck } from '../utils/utils.js';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 export let userMail;
 
-router.post("/", async (req, res) => {
-  const { email, password } = req.body;
+router.post("/", [
+  body('email').isEmail().withMessage('Invalid email format'),
+  body('password').isLength({ min: 6 }).notEmpty().withMessage('Password must be at least 6 characters long')
+],
+  async (req, res) => {
 
-  const q = "SELECT * FROM users WHERE email = ?";
-  const [data] = await pool.query(q, [email]);
+    const { email, password } = req.body;
 
-  if (data.length === 0) {
-    return res.status(401).json({ success: false, message: "Invalid email" });
-  }
+    const q = "SELECT * FROM users WHERE email = ?";
+    const [data] = await pool.query(q, [email]);
 
-  // Compare password hashes
-  try {
-    const isMatch = await passwordCheck(password, data[0].password);
-
-    if (isMatch) {
-      //Save mail to session to retreive in another route
-      // req.session.email = email;
-      userMail = email;
-
-      console.log("Mail from login route: ", userMail)
-      // returning admin value if true or false as this will affect the UI functions
-      return res.status(200).json({ success: true, user: data[0]['admin'] });
-    } else {
-      return res.status(401).json({ success: false, message: "Invalid email or password" });
+    if (data.length === 0) {
+      return res.status(401).json({ success: false, message: "Invalid email" });
     }
-  } catch (err) {
-    console.error("Error comparing passwords:", err);
-    return res.status(500).json({ error: "Error comparing password" });
-  }
-});
+
+    // Compare password hashes
+    try {
+      const isMatch = await passwordCheck(password, data[0].password);
+
+      if (isMatch) {
+        //Save mail to session to retreive in another route
+        // req.session.email = email;
+        userMail = email;
+
+        console.log("Mail from login route: ", userMail)
+        // returning admin value if true or false as this will affect the UI functions
+        return res.status(200).json({ success: true, user: data[0]['admin'] });
+      } else {
+        return res.status(401).json({ success: false, message: "Invalid email or password" });
+      }
+    } catch (err) {
+      console.error("Error comparing passwords:", err);
+      return res.status(500).json({ error: "Error comparing password" });
+    }
+  });
 
 export default router;
